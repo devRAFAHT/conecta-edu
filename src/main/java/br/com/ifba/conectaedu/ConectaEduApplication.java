1050d455dd16d70d59540b3879df87c078a7376a
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-12-03 11:35:27 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/ifba/conectaedu/ConectaEduApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.conectaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectaEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ifba/conectaedu/ConectaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.conectaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
