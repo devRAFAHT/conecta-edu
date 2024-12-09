3e15727e4ba461b9b90827eefd254b953d836a3c
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-12-08 21:35:26 -0300

    Fix: Adiciona o Mapper e o DTO de Administrador removido

---
+++ src/main/java/br/com/ifba/conectaedu/web/dto/AdministradorResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.ifba.conectaedu.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdministradorResponseDTO {
+    private Long usuarioId;
+    private Long escolaId;
+}
